a078b448e9926e686d99a2d8e40271f19f145f22
Tanmay <tanmaychoudhary4102@gmail.com>
2023-11-09 21:20:45 +0530

    dev1 first commit

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,40 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Header from "@/components/Header";
+import Home from "@/components/Home";
+import EmptyBoard from '@/components/EmptyBoard';
+import boardsSlice from "@/redux/boardsSlice";
 
-export default function Document() {
+function App() {
+  const [isBoardModalOpen, setIsBoardModalOpen] = useState(false);
+  const dispatch = useDispatch();
+  const boards = useSelector((state) => state.boards);
+  const activeBoard = boards.find((board) => board.isActive);
+  if (!activeBoard && boards.length > 0)
+    dispatch(boardsSlice.actions.setBoardActive({ index: 0 }));
   return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+    <div className=" overflow-hidden  overflow-x-scroll">
+      <>
+        {boards.length > 0 ?
+          <>
+            <Header
+              setIsBoardModalOpen={setIsBoardModalOpen}
+              isBoardModalOpen={isBoardModalOpen}
+            />
+            <Home
+              setIsBoardModalOpen={setIsBoardModalOpen}
+              isBoardModalOpen={isBoardModalOpen}
+            />
+          </>
+          :
+          <>
+            <EmptyBoard type='add' />
+          </>
+        }
+
+      </>
+    </div>
+  );
 }
+
+export default App;
